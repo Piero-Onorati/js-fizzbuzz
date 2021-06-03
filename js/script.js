376ff4dde11bb1cd2d757cb028c4e9443c87da31
Piero <piero.onorati@virgilio.it>
2021-06-03 14:59:15 +0200

    add correct algorithm

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 
 for (var i = 1; i <= 100; i++) {
 
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 != 0) {
         console.log(i + ' ' + 'Fizz');
         
-    } else if (i % 5 == 0) {
+    } else if (i % 5 == 0 && i % 3 != 0) {
         console.log(i + ' ' + 'Buzz');
         
     } else if (i % 3 == 0 && i % 5 == 0) {
