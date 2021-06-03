764bf364d0559e982206b0d00475c2f6e4e270ac
Piero <piero.onorati@virgilio.it>
2021-06-03 14:55:02 +0200

    add space

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@
 for (var i = 1; i <= 100; i++) {
 
     if (i % 3 == 0) {
-        console.log(i + 'Fizz');
+        console.log(i + ' ' + 'Fizz');
         
     } else if (i % 5 == 0) {
-        console.log(i + 'Buzz');
+        console.log(i + ' ' + 'Buzz');
         
     } else if (i % 3 == 0 && i % 5 == 0) {
-        console.log(i + 'FizzBuzz');
+        console.log(i + ' ' + 'FizzBuzz');
        
     }else{
         console.log(i);
